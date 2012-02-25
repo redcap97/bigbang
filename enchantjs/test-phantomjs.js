d48613abbf8b42eccb02d70de88f0729ccab9559
redcap97 <redcap97@gmail.com>
2012-02-25 13:38:03 +0900

    removed enchantjs files

--- enchantjs/test-phantomjs.js
+++
@@ -1,8 +0,0 @@
-if (phantom.state.length === 0) {
-    phantom.state = 'test-phantomjs';
-    phantom.open(phantom.args[0]);
-} else {
-    if (phantom.loadStatus === 'success') {
-        phantom.exit();
-    }
-}
