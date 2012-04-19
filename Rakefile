cee7a8f4e64f7cbe43af7e82aba494d4e27dcf05
redcap97 <redcap97@gmail.com>
2012-04-20 00:59:29 +0900

    fixed Rakefile to add the license code

--- Rakefile
+++ Rakefile
@@ -26,6 +26,34 @@ SOURCE_FILES = %w{
   src/main.coffee
 }
 
+LICENSE = <<EOS
+/*
+Copyright (C) 2012 Akira Midorikawa
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+EOS
+
+def add_license(file)
+  src = open(file).read
+  open(file, "w") do |f|
+    f.write LICENSE
+    f.write src
+  end
+end
+
 task :default => 'main.js'
 
 task :install do
@@ -35,4 +63,5 @@ end
 
 file 'main.js' => SOURCE_FILES do |t|
   sh "coffee --join #{t.name} --compile #{t.prerequisites.join(' ')}"
+  add_license(t.name)
 end
--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2012 Akira Midorikawa
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 (function() {
   var BattleGame, Blast, BlastRenderer, Block, BlockRenderer, Bomb, BombKick, BombRenderer, BombUp, Bomberman, BombermanRenderer, DataTransport, Direction, ENCHANTJS_IMAGE_PATH, EntryScreen, Field, FieldObject, FieldRenderer, FirePowerUp, GameResult, Ground, InitialNoticeRenderer, InputManager, Item, ItemRenderer, MAX_NUMBER_OF_PLAYERS, MESSAGE_DISCONNECT, Point, PressureBlock, PressureBlockRenderer, PressureBlockSetter, RESOURCES, Random, Rectangle, Remocon, Renderer, RenderingQueue, SpeedUp, TimerRenderer, WS_SUBPROTOCOL, Wall, WarningRenderer, createGameResult,
     __slice = Array.prototype.slice,
